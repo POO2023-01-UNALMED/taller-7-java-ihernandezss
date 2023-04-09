ec0ca79f485cecf05c0f3e34cd4795e691b9be3f
ihernandezss <118498504+ihernandezss@users.noreply.github.com>
2023-04-09 18:05:44 -0500

    Create Fabula.java

---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,37 @@
+package comunicacion;
+
+public class Fabula extends Escrito{
+private String ensenanza;
+private String interpretacion;
+	
+//constructor
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+		super (origen, titulo, autor, paginas);
+		this.ensenanza=ensenanza;
+		this.interpretacion=interpretacion;
+	}
+//getters y setters
+	public String getEnsenanza() {
+		return ensenanza;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza=ensenanza;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion=interpretacion;}
+	
+//metodos
+	public int palabrasTotales(int palabrasPagina) {
+		return getPaginas()*palabrasPagina;
+	}
+	public String interpretacion() {
+		return interpretacion;
+	}
+	public String toString () {
+		return getOrigen()+"\n"+getTitulo()+"\n"+getAutor()+"\n"+getPaginas()+"\n"+ensenanza+"\n"+interpretacion+"\n";
+	}
+
+}
